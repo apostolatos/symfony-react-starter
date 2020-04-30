591403191c2b14b3de3f854fbbe665167dd22f84
Dimitris Apostolatos <dimitris@CloudCoreOne.com>
2020-04-30 13:51:06 +0300

    add missing css files

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,6 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .enableReactPreset()
-
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
@@ -71,6 +69,8 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+
+    .enableReactPreset()
 ;
 
 module.exports = Encore.getWebpackConfig();
